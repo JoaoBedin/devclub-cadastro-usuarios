fc6a0b5857968d2ae27faca002ab1db889c51768
joaobigodon <prestarcs61@gmail.com>
2025-08-07 15:51:18 -0300

    Project-Cadastro-de-Usuarios

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Container, Title } from "./styles"
+function App() {
+  
+
+  return (
+
+    
+      
+      <Container>
+     
+        <Title>Olá React</Title>
+      </Container>
+         
+ 
+ )
+}
+
+export default App
+
+/*  Exportar "Padrão"
+    Apenas Exportar
+*/
