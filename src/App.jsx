291980526140868f8c6e73817c8c3b433d76678c
joaobigodon <prestarcs61@gmail.com>
2025-08-11 13:52:24 -0300

    html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Container, Title } from "./styles"
-function App() {
+import { Container, Form, Title } from "./styles"
+function Home() {
   
 
   return (
@@ -7,8 +7,38 @@ function App() {
     
       
       <Container>
+      <TopBackground>
+
+      <img src="" alt="" />
+      </TopBackground>
+
      
-        <Title>Olá React</Title>
+     <Form>
+<Title>Cadastrar Usuários</Title>
+
+<ContainerInputs>
+<div>
+  <div>
+  <InputLabel>Nome <span>*</span></InputLabel>
+<Input type="text" placeholder="Nome do usuário"> </Input>
+</div>
+
+<div>
+  <InputLabel>Idade<span>*</span></InputLabel>
+<Input type="number" placeholder="Idade do Usuário"> </Input>
+</div>yarn run dev
+
+<div>
+ <InputLabel>Email<span>*</span></InputLabel>
+<Input type="email" placaholder="Email do usuário"> </Input>
+</div>
+
+</div>
+</ContainerInputs>
+<Button> Cadastrar Usuário</Button>
+
+     </Form>
+        
       </Container>
          
  
--- src/styles.js
+++ src/styles.js
@@ -9,3 +9,21 @@ border-color: aqua;
 export const Container = styled.div `
 
 `
+
+export const TopBackground = styled.div `
+
+`
+export const ContainerInputs = styled.div `
+
+`
+export const Input = styled.input `
+
+`
+export const Form = styled.form `
+
+`
+export const Button = styled.button `
+
+`
+export const InputLabel = styled.label`
+`
