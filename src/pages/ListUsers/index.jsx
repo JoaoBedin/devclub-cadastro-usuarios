623395cb15da908d38b87003dedd7ad06a75904a
JoaoBedin <prestarcs61@gmail.com>
2025-08-28 16:13:22 -0300

    useEffect adicionado para pegar todos os usuarios

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,10 +1,21 @@
 import Button from '../../components/Button'
 import { useNavigate } from 'react-router-dom'
 import api from '../../services/api.js'
+import { useEffect } from 'react'
 import TopBackground from '../../components/TopBackground'
 
 
 function ListUsers(){
+
+useEffect(()=>{
+
+async function getUsers(){
+const usersFromApi = await api.get("/usuarios")
+  console.log(usersFromApi.data)
+  } 
+
+getUsers()
+ } , [])   
 
 
 const navigate = useNavigate()
